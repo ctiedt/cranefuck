4973200ca34e7052cf60ec1d463f6b56f0fa3594
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2023-01-06 17:14:44 +0100

    update print_bf script, move examples

--- print_bf.py
+++ print_bf.py
@@ -1,10 +1,21 @@
+#!/usr/bin/python
+# A simple tool to generate a program to print a brainfuck string
 import sys
 
 def print_bf(s: str):
+    special = False
     for char in s:
-        rep = ord(char)
+        if char == "\\":
+            special = True
+            continue
+        if special:
+            if char == "n":
+                rep = ord("\n")
+        else:
+            rep = ord(char)
         print("+" * rep, end="")
         print(".>")
+        special = False
 
 if __name__ == "__main__":
     print_bf(sys.argv[1])
