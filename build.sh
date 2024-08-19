0d1b265ab3c73daf9d1977a4d29c3df07e82b6ee
Sam Estep <sam@samestep.com>
2024-08-19 12:06:17 -0400

    Fix `build.sh`

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cargo run --release -- $1
-if [-f /lib/crt1.o]; then
+cargo run --release -- "$1"
+if [ -f /lib/crt1.o ]; then
     ld -o a /lib/crt1.o a.out -lc
-elif [-f /usr/lib32/crt1.o]; then
+elif [ -f /usr/lib32/crt1.o ]; then
     ld -o a /usr/lib32/crt1.o a.out -lc
 else
     echo "No crt1.o found"
